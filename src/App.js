cbfb25c8a0980ab603b725ec3131c6df6799c733
Taher Mulla <taher.mulla@gmail.com>
2023-11-11 21:03:16 -0500

    added IP desiplay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 // src/App.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css"; // Import a separate CSS file for styling
 
 function App() {
   const [randomNumber, setRandomNumber] = useState(null);
+  const [ipAddress, setIpAddress] = useState(null);
+
+  useEffect(() => {
+    // Fetch IPv4 address when the component mounts
+    axios.get("https://api.ipify.org?format=json").then((response) => {
+      setIpAddress(response.data.ip);
+    });
+  }, []);
 
   const generateRandomNumber = () => {
     const newRandomNumber = Math.floor(Math.random() * 100) + 1;
     setRandomNumber(newRandomNumber);
   };
-
   return (
     <div className="app-container">
       <p className="greeting">Hi, my name is Taher</p>
+      {/* <p className="ip-address">Your IP address is: {ipAddress}</p> */}
       <button className="random-button" onClick={generateRandomNumber}>
-        Generate Random Number
+        What is my ip?
       </button>
-      {randomNumber && (
-        <p className="random-number">Random Number: {randomNumber}</p>
-      )}
+      {randomNumber && <p className="random-number">{ipAddress}</p>}
     </div>
   );
 }
